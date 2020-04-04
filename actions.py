7489f16dda08781cfed163fba02b1acaa91dd7d8
Vimannyu <55655723+Vimannyu@users.noreply.github.com>
2020-04-04 14:05:21 +0530

    Add files via upload

--- actions.py
+++ actions.py
@@ -25,3 +25,32 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from rasa_sdk.forms import FormAction
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+
+class SalesForm(FormAction):
+    """Collects sales information and adds it to the spreadsheet"""
+
+    def name(self):
+        return "sales_form"
+    @staticmethod
+    def required_slots(tracker):
+        return [
+            "job_function",
+            "use_case",
+            "budget",
+            "person_name",
+            "company",
+            "business_email",
+            ]
+    def submit(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        dispatcher.utter_message("Thanks for getting in touch, we’ll contact you soon")
+        return []
